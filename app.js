b3ecf6768ff38e584dd9a5bb83cb78821576fcda
Jatin Yadav <jatinyadav.jatin.29@gmail.com>
2025-09-06 20:11:10 +0530

    Adding files and implementing Error handeling

--- app.js
+++ app.js
@@ -4,22 +4,31 @@ const methodOverride = require("method-override");
 const path = require("path");
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const expressLayouts = require("express-ejs-layouts");
+const wrapAsync = require("./utils/wrapAsync.js");
+const expressError = require("./utils/expressError.js");
+
 let port = 3006;
 
+app.use(expressLayouts);
+app.set("layout", "layouts/boilerplate"); // default layout
 app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.static(path.join(__dirname, "public")));
 
+// Listning to port
 app.listen(port, () => {
   console.log(`Connected to port : ${port}`);
 });
 
+// default route
 app.get("/", (req, res) => {
   res.send("welcome 😃");
 });
 
+// connecting to mongoose
 main()
   .then(() => {
     console.log("connected");
@@ -32,56 +41,92 @@ async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/airbnb");
 }
 
-app.get("/listings", async (req, res) => {
-  let allListings = await Listing.find({});
-  res.render("listings/index.ejs", { allListings });
-});
+// All listings
+app.get(
+  "/listings",
+  wrapAsync(async (req, res) => {
+    let allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+  })
+);
 
+// Creating new Listings
 app.get("/listings/new", (req, res) => {
   res.render("listings/new.ejs");
 });
 
-app.post("/listings/new", async (req, res) => {
-  const newListing = new Listing(req.body.listing);
-  await newListing.save();
-  res.redirect("/listings");
-});
+app.post(
+  "/listings/new",
+  wrapAsync(async (req, res, next) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+  })
+);
 
-app.get("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  let details = await Listing.findById(id);
-  res.render("listings/view.ejs", { details });
-});
+// Detailed View of listing
+app.get(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let details = await Listing.findById(id);
+    res.render("listings/view.ejs", { details });
+  })
+);
 
-app.get("/listings/:id/edit", async (req, res) => {
-  let { id } = req.params;
-  let details = await Listing.findById(id);
-  res.render("listings/edit.ejs", { details });
-});
+// Edit Listing
+app.get(
+  "/listings/:id/edit",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let details = await Listing.findById(id);
+    res.render("listings/edit.ejs", { details });
+  })
+);
 
-app.patch("/listings/:id/edit", async (req, res) => {
-  let { id } = req.params;
-  let {
-    location: formLocation,
-    country: formCountry,
-    description: formDescription,
-    price: fromPrice,
-  } = req.body;
-  let newDetails = await Listing.findByIdAndUpdate(
-    id,
-    {
+app.patch(
+  "/listings/:id/edit",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let {
       location: formLocation,
       country: formCountry,
       description: formDescription,
       price: fromPrice,
-    },
-    { runValidators: true, new: true }
-  );
-  res.redirect("/listings");
-});
+    } = req.body;
+    let newDetails = await Listing.findByIdAndUpdate(
+      id,
+      {
+        location: formLocation,
+        country: formCountry,
+        description: formDescription,
+        price: fromPrice,
+      },
+      { runValidators: true, new: true }
+    );
+    res.redirect("/listings");
+  })
+);
 
 app.delete("/listings/:id", async (req, res) => {
   let { id } = req.params;
   let deleteListings = await Listing.findByIdAndDelete(id);
   res.redirect("/listings");
+});
+
+//page not found
+app.use((req, res, next) => {
+  next(new expressError(404, "Page not found"));
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message = "Something went wrong" } = err;
+  res.status(statusCode).send(message);
+});
+
+// error Handeling
+app.use((err, req, res, next) => {
+  let { statusCode, message } = err;
+  res.statusCode(statusCode).send(message);
 });
